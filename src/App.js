ee066bddd8c3419c4e823315406ad2ed8cd94179
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-08-12 22:36:11 +0530

    form header updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import { Balance } from './components/Balance.js';
 import { Header } from './components/Header.js';
 
 function App() {
   return (
     <div>
      <Header/>
+     <div className='container'>
+      <Balance/>
+     </div>
     </div>
   );
 }
