ebaeb7ff62a7fab2849f3b86a1c1f09b80711a76
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-08-12 23:17:34 +0530

    set use state on AddTransaction

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 import './App.css';
+import { AddTransaction } from './components/AddTransaction.js';
 import { Balance } from './components/Balance.js';
 import { Header } from './components/Header.js';
+import { IncomeExpenses } from './components/IncomeExpenses.js';
+import { TransactionList } from './components/TransactionList.js';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
      <Header/>
      <div className='container'>
       <Balance/>
+      <IncomeExpenses/>
+      <TransactionList/>
+      <AddTransaction/>
      </div>
     </div>
   );
